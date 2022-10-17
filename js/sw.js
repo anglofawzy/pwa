f59c17c8fd00376b3a9ed849ae6cbd67ea3b903e
Anglo Fawzy <anglo.fawzy14@gmail.com>
2022-10-18 00:41:49 +0200

    final final

--- js/sw.js
+++ js/sw.js
@@ -1,11 +1,15 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('enter app').then((cache) => cache.addAll([
+      '/pwa',
       '/pwa/index.html',
       '/pwa/signUp.html',
       '/pwa/js/main.js',
+      '/pwa/js/app.js',
+      '/pwa/js/particles.min.js',
       '/pwa/css/style.css',
-      '/pwa/css/media.css'
+      '/pwa/css/media.css',
+      '/pwa/assets/particles.json'
     ])),
   );
 });
