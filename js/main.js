69b53a0c09a99ba81f8c1675dbe0463981e55f5d
Anglo Fawzy <anglo.fawzy14@gmail.com>
2022-10-18 15:48:31 +0200

    final edit

--- js/main.js
+++ js/main.js
@@ -122,12 +122,15 @@ window.addEventListener('beforeinstallprompt', (e) => {
     deferredPrompt = e;
     // Update UI to notify the user they can add to home screen
     addBtn.style.display = 'block';
+    // Optionally, send analytics event that PWA install promo was shown.
+    console.log(`'beforeinstallprompt' event was fired.`);
   
     addBtn.addEventListener('click', () => {
       // hide our user interface that shows our A2HS button
       addBtn.style.display = 'none';
       // Show the prompt
       deferredPrompt.prompt();
+      console.log(deferredPrompt.choiceResult);
       // Wait for the user to respond to the prompt
       deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
