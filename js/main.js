c34d1d01f670df3debb17f2e4804e0efdac20a23
Anglo Fawzy <90111639+anglofawzy@users.noreply.github.com>
2022-10-31 22:20:17 +0200

    delete pompt

--- js/main.js
+++ js/main.js
@@ -104,46 +104,42 @@ async function  postApi(data, endPoint) {
 // Register service worker to control making site work offline
 
 if ('serviceWorker' in navigator) {
-    try {
         navigator.serviceWorker.register('/pwa/sw.js').then(() => {console.log('test service');})    
-    } catch (error) {
-        console.log('error');
-    }
-        
+        console.log('done');
 }
   
   // Code to handle install prompt on desktop
   
-let deferredPrompt;
-const addBtn = document.querySelector('#pwa');
-addBtn.style.display = 'none';
+// let deferredPrompt;
+// const addBtn = document.querySelector('#pwa');
+// addBtn.style.display = 'none';
   
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    addBtn.style.display = 'block';
-    // Optionally, send analytics event that PWA install promo was shown.
-    console.log(`'beforeinstallprompt' event was fired.`);
+// window.addEventListener('beforeinstallprompt', (e) => {
+//     // Prevent Chrome 67 and earlier from automatically showing the prompt
+//     e.preventDefault();
+//     // Stash the event so it can be triggered later.
+//     deferredPrompt = e;
+//     // Update UI to notify the user they can add to home screen
+//     addBtn.style.display = 'block';
+//     // Optionally, send analytics event that PWA install promo was shown.
+//     console.log(`'beforeinstallprompt' event was fired.`);
 
-    addBtn.addEventListener('click', () => {
-        // hide our user interface that shows our A2HS button
-        addBtn.style.display = 'none';
-        // Show the prompt
-        deferredPrompt.prompt();
-        console.log(deferredPrompt.choiceResult);
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
-          }
-          deferredPrompt = null;
-        });
-      });
+//     addBtn.addEventListener('click', () => {
+//         // hide our user interface that shows our A2HS button
+//         addBtn.style.display = 'none';
+//         // Show the prompt
+//         deferredPrompt.prompt();
+//         console.log(deferredPrompt.choiceResult);
+//         // Wait for the user to respond to the prompt
+//         deferredPrompt.userChoice.then((choiceResult) => {
+//           if (choiceResult.outcome === 'accepted') {
+//             console.log('User accepted the A2HS prompt');
+//           } else {
+//             console.log('User dismissed the A2HS prompt');
+//           }
+//           deferredPrompt = null;
+//         });
+//       });
   
     
-});
+// });
